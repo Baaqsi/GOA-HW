8b0475da222d11866e8e90fea9794be43d798aea
Baaqsi <lukagamera2010@gmail.com>
2024-05-31 20:31:33 +0400

    second commit

--- day 00/homework/davaleba.py
+++
@@ -1,6 +0,0 @@
-name = "luka"
-surname = "lomidze"
-age = 14
-height = 169.5
-
-print("me var " + name + " " + surname +  "\n" + "var " + str(age) + " wlis " + "var " + str(height) + "m")
---
+++ day 05/davaleba.py
@@ -0,0 +1,9 @@
+#1
+#name = "luka"
+#surnamne = "lomidze"
+#age = 14
+#address = "mukhianis dachebi tergis qucha n4"
+#print(f"saxeli : {name} , gvari: {surnamne} , asaki: {age} , misamarti: {address}")
+
+#2
+num1 = 1
